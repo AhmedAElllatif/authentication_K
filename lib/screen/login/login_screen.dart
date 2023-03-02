56d5b8236520736474e772a3c91b950df95634d8
kareemElsayedEltantawy <kareem.elsayed@excp.sa>
2023-03-02 16:06:34 +0200

    login

--- lib/screen/login/login_screen.dart
+++ lib/screen/login/login_screen.dart
@@ -1,15 +1,40 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:poffeh_app/components/text/custom_text.dart';
+import 'package:poffeh_app/helper/cache_helper.dart';
 import 'package:poffeh_app/routes/routes.dart';
+import 'package:poffeh_app/screen/login/cubit/cubit.dart';
+import 'package:poffeh_app/screen/login/cubit/states.dart';
 import 'package:poffeh_app/utility/app_theme.dart';
 import '../../components/button/button.dart';
+import '../../components/text_field/text_field_custom.dart';
+import '../../helper/validation.dart';
+import '../../utility/all_app_words.dart';
 
 class LoginScreen extends StatelessWidget {
   static const routeName ='LoginScreen';
+  var nameController = TextEditingController();
+  var passwordController = TextEditingController();
+  var formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
+    return BlocProvider(
+  create: (context) => LoginCubit(),
+  child: BlocConsumer<LoginCubit, LoginStates>(
+  listener: (context, state) {
+    if(state is LoginSuccessState){
+      if(state.loginModel.state!){
+        CacheHelper.saveData(key: 'token', value: state.loginModel.data!.token!.accessToken!).then((value) {
+          goToScreen(screenNames: ScreenNames.defaultscreen);
+        });
+      }else{
+        print(state.loginModel.message![0].value!);
+      }
+    }
+ },
+  builder: (context, state) {
     return Scaffold(
       appBar: AppBar(
         leading: Padding(
@@ -31,27 +56,81 @@ class LoginScreen extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.all(14.0),
           child: SingleChildScrollView(
-            child: Column(
-              children: [
-                CustomText(text: 'مرحبا بك', color: AppColors.mainColor, fontSize: 20.sp, fontFamily: AppFonts.fontBold),
-                Padding(
-                  padding: const EdgeInsets.all(12.0),
-                  child: CustomText(text: 'سجل دخولك بواسطة اسم المستخدم وكلمة السر وتمتع معنا بتطبيق المطبخ', color: AppColors.black, fontSize: 14.sp, fontFamily: AppFonts.fontBold,textAlign: TextAlign.center,),
-                ),
+            child: Form(
+              key: formKey,
+              child: Column(
+                children: [
+                  CustomText(text: LLogin.welcome, color: AppColors.mainColor, fontSize: 20.sp, fontFamily: AppFonts.fontBold),
+                  Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: CustomText(text: LLogin.welcome_desc, color: AppColors.black, fontSize: 14.sp, fontFamily: AppFonts.fontBold,textAlign: TextAlign.center,),
+                  ),
+                  SizedBox(
+                    height: 15.h,
+                  ),
+                  CustomTextField(
+                    controller: nameController,
+                    hintText: LLogin.user_name,
+                    labelText: '',
+                    inputType: TextInputType.name,
+                    textFieldVaidType: TextFieldvalidatorType.DisplayText,
+                  ),
 
-                CustomButton(
-                  fontSize: 14.sp,
-                  color: AppColors.mainColor,
-                  text:'تسجيل الدخول',
-                  onPressed: (){
-                  goToScreenpopAndPushNamed(screenNames: ScreenNames.loginScreen);
-                },)
+                  CustomTextField(
+                    controller: passwordController,
+                    hintText: LLogin.password,
+                    labelText: '',
+                    obscureText: true,
+                    inputType: TextInputType.text,
+                    textFieldVaidType: TextFieldvalidatorType.Password,
+                  ),
+                  SizedBox(
+                    height: 20.h,
+                  ),
+                  state is! LoginLoadingState ?  CustomButton(
+                    fontSize: 14.sp,
+                    color: AppColors.mainColor,
+                    text:LLogin.login,
+                    onPressed: (){
+                    if(formKey.currentState!.validate()){
+                      LoginCubit.get(context).login(userName: nameController.text, password: passwordController.text, rememberMe: 'true');
+//                      goToScreen(screenNames: ScreenNames.defaultscreen);
+                    }
+                  },) : CircularProgressIndicator(),
+                  SizedBox(
+                    height: 20.h,
+                  ),
+                  InkWell(
+                    onTap: (){
 
-              ],
+                    },
+                    child: CustomText(text: LLogin.forget_password, color: AppColors.Kbluecolor, fontSize: 14.sp,
+                        fontFamily: AppFonts.fontMedium),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 14.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        CustomText(text: LLogin.haveAccount, color: AppColors.black, fontSize: 14.sp,
+                            fontFamily: AppFonts.fontMedium),
+                        SizedBox(width: 5.w,),
+                        CustomText(text: LLogin.admin, color: AppColors.mainColor, fontSize: 14.sp,
+                            fontFamily: AppFonts.fontMedium),
+                      ],
+                    ),
+                  )
+
+
+                ],
+              ),
             ),
           ),
         ),
       ),
     );
+  },
+),
+);
   }
 }
--- lib/utility/all_app_words.dart
+++ lib/utility/all_app_words.dart
@@ -1,8 +1,19 @@
 part 'app_images.dart';
 
-class LLogin {}
+class LLogin {
+  static const String user_name = 'user name';
+  static const String password = 'password';
+  static const String login = 'login';
+  static const String welcome = 'مرحبا بك';
+  static const String welcome_desc = 'سجل دخولك بواسطة اسم المستخدم وكلمة السر وتمتع معنا بتطبيق المطبخ';
+  static const String forget_password = 'forget password';
+  static const String haveAccount ='Do you have company';
+  static const String admin = 'login as admin';
+}
 
-class GlobalWords {}
+class GlobalWords {
+
+}
 
 class AlertType {
   static const String alert = 'alertType_alert';
@@ -12,4 +23,6 @@ class AlertType {
   static const String sorry = 'alertType_sorry';
 }
 
-class ErrorMessage {}
+class ErrorMessage {
+
+}
